9faf6200c262d82ac94451a9e659aa05ba4cb652
melody-omoro2 <melody2omoro@gmail.com>
2023-09-11 05:18:38 +0100

    edit

--- script.js
+++ script.js
@@ -7,34 +7,14 @@ const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fri
         // Update the content of the 'currentDayOfTheWeek' element
         document.getElementById("dayOfWeek").textContent = currentDayOfWeek;
 
-            // Update the current time
-            function updateCurrentTime() {
-            const currentHourElement = document.getElementById("currentHour");
-            const currentMinuteElement = document.getElementById("currentMin");
-            const currentSecondsElement = document.getElementById("currentSecs");
-            const currentDate = new Date();
-            const currentUTCHour = currentDate.getHours(); // Get current hour
-            const currentUTCMin = currentDate.getMinutes(); // Get current minutes
-            const currentUTCSecs = currentDate.getSeconds(); // Get current seconds
-
-            // Update the content of the 'currentUTCTime' element
-            currentHourElement.textContent = currentUTCHour;
-            currentMinuteElement.textContent = currentUTCMin;
-            currentSecondsElement.textContent = currentUTCSecs;
+        function getCurrentUtcTimestamp() {
+            return Date.now();
         }
 
-        function updateTime() {
-            const clockElement = document.getElementById('timeOfTheDay');
-            const now = new Date();
-            const hours = String(now.getHours()).padStart(2, '0');
-            const minutes = String(now.getMinutes()).padStart(2, '0');
-            const seconds = String(now.getSeconds()).padStart(2, '0');
-            const currentTime = `${hours}:${minutes}:${seconds}`;
-            clockElement.textContent = currentTime;
+        function updateTimestamp() {
+            const currentUtcTimestamp = getCurrentUtcTimestamp();
+            document.getElementById("timesOfTheDay").textContent = currentUtcTimestamp;
         }
 
-        // Update the time every second
-        setInterval(updateTime, 1000);
-
-        // Initial call to display the time immediately
-        updateTime();
+        // Update the timestamp every second (1000 milliseconds)
+        setInterval(updateTimestamp, 1000);
